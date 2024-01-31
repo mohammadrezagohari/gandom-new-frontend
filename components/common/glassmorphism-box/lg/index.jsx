20adcb055940d46709bd144cd854d53a5e609fca
mohammadrezagohari <eng.mr.gohari@gmail.com>
2024-01-31 15:03:38 +0330

    update data before

--- components/common/glassmorphism-box/lg/index.jsx
+++ components/common/glassmorphism-box/lg/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 import { GoArrowDownLeft } from "react-icons/go";
-function GlassmorphismBox({title,desc}) {
+function GlassmorphismBox({ title, desc, link = null }) {
   return (
     <div>
       <div className="glassBox w-[278.92px] min-h-[160.01px] lg:w-[37.337239583333336vw] lg:h-[21.419270833333332vw]  p-4 pb-[15%] lg:p-7 rounded-md relative">
-          <h3 className="text-gf mb-[1%] lg:mb-[5%] lg:text-[32px] lg:leading-[36.16px] text-xl leading-[40.68px] font-Holispay ">{title}</h3>
-          <p className="text-gf text-justify lg:text-[18px] lg:leading-[27px] text-[14px] leading-[21px] font-PoppinsLight ">{desc}</p>
-        {/* <div className="absolute -bottom-5 right-4 lg:border-4 border-4 boreder-g21 w-[30.64px] h-[30.64px] lg:w-[63px] lg:h-[63px] rounded-full  border-g21 flex justify-center items-center bg-gYellow lg:text-2xl text-xl">
-              <GoArrowDownLeft className="transform rotate-180" />
-        </div> */}
+        <h3 className="text-gf mb-[1%] lg:mb-[5%] lg:text-[32px] lg:leading-[36.16px] text-xl leading-[40.68px] font-Holispay ">
+          {title}
+        </h3>
+        <p className="text-gf text-justify lg:text-[18px] lg:leading-[27px] text-[14px] leading-[21px] font-PoppinsLight ">
+          {desc}
+        </p>
+        <div className="absolute -bottom-5 right-4 lg:border-4 border-4 boreder-g21 w-[30.64px] h-[30.64px] lg:w-[63px] lg:h-[63px] rounded-full  border-g21 flex justify-center items-center bg-gYellow lg:text-2xl text-xl">
+          <a href={link??""} target="_blank">
+            <GoArrowDownLeft className="transform rotate-180" />
+          </a>
+        </div>
         {/* <div style={{border:'1px solid rgba(199, 199, 199, 0.68)',}} className=" absolute -bottom-7 right-8 lg:-bottom-14 lg:right-10 w-[45.64px] h-[45.64px] lg:w-[93px] lg:h-[93px] bg-transparent rounded-full flex justify-center items-center">
           <div className="w-[30.64px] h-[30.64px] lg:w-[63px] lg:h-[63px] rounded-full flex justify-center items-center  bg-gYellow">
             <GoArrowDownLeft className="transform rotate-180 lg:text-2xl text-xl" />
@@ -16,7 +22,7 @@ function GlassmorphismBox({title,desc}) {
         </div> */}
       </div>
     </div>
-  )
+  );
 }
 
-export default GlassmorphismBox
+export default GlassmorphismBox;
